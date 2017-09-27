9a90a3c32b6fd75f3c2365f585770c56df0b4ab2
Nick Pozoulakis <nicholaspoz@gmail.com>
2017-09-26 22:40:18 -0700

    added clicky goodness

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 (function() {
-  //alert("test");
+  var poz = $('#poz');
+  var body = $("#b");
+  var suggestion = $('#suggestion')
+  var count = 0;
+
+  //suggestion.hide();
+
+  body.on('click', function(event) {
+    var target = $(event.target)
+    if (!target.is('i')) {
+      count = count + 1;
+      poz.css('color', randomColor());
+    }
+    if (count === 7) {
+      suggestion.show();
+    }
+    if (count === 14) {
+      suggestion.text(suggestion.text().toUpperCase());
+    }
+  });
+
+  function randomColorVal() {
+    return 30 + Math.round(Math.random() * 69);
+  }
+
+  function randomColor() {
+    return '#' + [randomColorVal(), randomColorVal(), randomColorVal()].join('');
+  }
 })();
