2ec4b5570d1cc069a40cbcf8713ce8ef5061844b
Nick Pozoulakis <nicholaspoz@gmail.com>
2017-09-27 13:28:42 -0700

    script cleanup

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 (function() {
-  var poz = $('#poz');
-  mediaLinks = $("a.media")
   var body = $("#b");
-  var suggestion = $('#suggestion')
+  var poz = $('#poz');
+  var mediaLinks = $("a.media");
+  var suggestion = $('#suggestion');
   var count = 0;
 
-  //suggestion.hide();
-
   body.on('click', function(event) {
-    var target = $(event.target)
+    target = $(event.target);
     if (!target.is('i')) {
       count = count + 1;
       var color = randomColor()
       poz.css('color', color);
       $.each(mediaLinks, function(index, link) {
-        $(link).hover(function(hoverEvent){
-          $(this).css('color', hoverEvent.type === 'mouseenter' ? color : 'black');
+        $(link).hover(function(hoverEvent) {
+          var linkColor = hoverEvent.type === 'mouseenter' ? color : 'black'
+          $(this).css('color', linkColor);
         })
       });
     }
